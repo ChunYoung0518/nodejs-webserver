fb188bf84108685ee0fc443aff6be0edf9f2e8e0
chunyoung0518@gmail.com <chunyoung0518@gmail.com>
2021-08-11 20:06:05 +1000

    environment variables

--- Testing/src/index.js
+++ Testing/src/index.js
@@ -1,4 +1,13 @@
 import app from "./app";
+require("dotenv").config();
+
+if(process.env.NODE_ENV === "development") {
+    console.log("development environment");
+} else {
+    console.log("no environment variable set");
+}
+
+console.log(process.env.DATABASE);
 
 app.listen(3000, ()=>{
     console.log(`Server directory at http://localhost:3000`);
